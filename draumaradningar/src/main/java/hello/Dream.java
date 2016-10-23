51202f694504e4593cb84b901a540fcf8bc84a0c
Sigurgeir <sio46@hi.is>
2016-10-23 16:44:23 +0000

    Vann i Dream og User klosunum

--- draumaradningar/src/main/java/hello/Dream.java
+++ draumaradningar/src/main/java/hello/Dream.java
@@ -1,50 +1,65 @@
 package DreamDiary;
-
+import java.util.*;
 public class Dream {
 
-    private String date;
     private String content;
-    private String answer;
+    private int id;
+    private Date dreamDate;
+    //private Interpretation interpretation;
+    private String interpretation;
+    private int userId;
 
-    /*
-    public Dream(String date, String content, String answer) {
-        this.date = date;
+    public Dream(int dreamId, int userId, String content) {
+        this.id = dreamId;
+        this.userId = userId;
         this.content = content;
-        this.answer = answer;
-    }
-    */
-
-    public Dream(String date, String content) {
-        this.date = date;
-        this.content = content;
+        this.dreamDate = new java.util.Date();
     }
 
-    public Dream(){};
-
-    public String getDate() {
-        return date;
+    public void test(){
+      System.out.print("Draumur virkar");
     }
 
-    public void setDate(String date) {
-        this.date = date;
+    // Notkun: inputDream.interpret()
+    // Fyrir: inputDream er hlutur af tagi Dream.
+    // Eftir: inputDream hefur fengid Interpretation attribute
+    protected void interpret() {
+      this.interpretation = (new StringBuilder(this.content).reverse().toString());
     }
 
-    public String getContent() {
-        return content;
+    // Notkun: editDream(old_dream)
+    // Fyrir: old_dream er hlutur af tagi Dream
+    // Eftir:
+    // Liklega redundant. Notum bara setContent
+    //protected void editDream(Dream old_dream) {}
+
+
+    // Only getters and setters bellow this point
+    public String getContent(){
+      return content;
     }
 
-    public void setContent(String content) {
-        this.content = content;
+    public void setContent(String dreamText){
+      this.content=dreamText;
     }
 
-    /*
-    public String getAnswer() {
-        return answer;
+    public int getId() {
+      return id;
     }
 
-    public void setAnswer(String answer) {
-        this.answer = answer;
+    public void setId(int new_dreamId){
+      this.id = new_dreamId;
     }
-    */
 
+    public int getUserId(){
+      return userId;
+    }
+
+    public void setUserId(int new_userId){
+      this.userId = new_userId;
+    }
+
+    public Date getDate() {
+      return dreamDate;
+    }
 }
--- draumaradningar/src/main/java/hello/User.java
+++ draumaradningar/src/main/java/hello/User.java
@@ -1,9 +1,75 @@
-package DreamDiary;
+import java.util.*;
+
+//package DreamDiary;
 
 public class User {
 
   private String name;
   private int id;
   private String password;
+  protected List<Dream> dreamList = new ArrayList<Dream>();
+
+  // Constructor
+  public User(String userName, int userId, String userPassword) {
+    this.name = userName;
+    this.id = userId;
+    this.password = userPassword;
+  }
+
+
+  // Notkun: logDream(old_Dream_list, dream_text)
+  // Fyrir: dream_text er texti draumsins, fenginn ur forum submission
+  //        dream_text er a islensku(?)
+  //
+  // Eftir: Nytt Dream object hefur verid buid til og baett vid old_Dream_list
+  //        nyja Dream objectid
+  /*protected List<Dream> logDream(int dreamId, List<Dream> oldDreamList, String dream_text) {
+    Dream nyr_draumur = new Dream(dreamId, dream_text);
+    List<Dream> updatedDreamList = oldDreamList.add(nyr_draumur);
+    return updatedDreamList;
+  }*/
+  protected Dream logDream(int dreamId, List<Dream> oldDreamList, String dream_text) {
+    Dream nyr_draumur = new Dream(id, dreamId, dream_text);
+    return nyr_draumur;
+  }
+
+
+  // Notkun:
+  // Fyrir:
+  // Eftir:
+  protected void deleteDream(List<Dream> my_dreams) {
+
+  }
+
+  // possibly redundant
+  protected void updateUserInfo(String name, int id, String password){
+
+  }
+
+
+  // Only getters and setters bellow here:
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String new_name) {
+    this.name = new_name;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int new_id) {
+    this.id = new_id;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String new_password) {
+    this.password = new_password;
+  }
 
 }
