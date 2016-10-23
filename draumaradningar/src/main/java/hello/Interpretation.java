3fce1bd0c34b105b2cc4c1ab0918532305978624
Sigurgeir <sio46@hi.is>
2016-10-23 11:59:12 +0000

    bara ad koma mer af stad. Bjo til User klasa

--- draumaradningar/src/main/java/hello/Interpretation.java
+++ draumaradningar/src/main/java/hello/Interpretation.java
@@ -1,4 +1,4 @@
-package hello;
+package DreamDiary;
 
 public class Interpretation {
 
---
+++ draumaradningar/src/main/java/hello/User.java
@@ -0,0 +1,9 @@
+package DreamDiary;
+
+public class User {
+
+  private String name;
+  private int id;
+  private String password;
+
+}
