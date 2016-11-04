2735f21be94ad1a299b79af9ada4343e98949df5
Haraldur Gretarsson <halligret@gmail.com>
2016-11-04 23:24:17 +0000

    Bætti vid virkni fyrir facebook login, vantar samt appID i skranna /src/resources/application.properties

--- draumaradningar/src/main/java/DreamDiary/UserController.java
+++ draumaradningar/src/main/java/DreamDiary/UserController.java
@@ -1,5 +1,6 @@
 package DreamDiary;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,18 +10,31 @@ import javax.validation.Valid;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.PagedList;
+import org.springframework.social.facebook.api.Post;
 
 @Controller
+@RequestMapping("/login")
 public class UserController extends WebMvcConfigurerAdapter {
 	
+	private Facebook facebook;
+    private ConnectionRepository connectionRepository;
+
+    public UserController(Facebook facebook, ConnectionRepository connectionRepository) {
+        this.facebook = facebook;
+        this.connectionRepository = connectionRepository;
+	}
+	/*
 	@Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/result").setViewName("result");
     }
+	*/
 	
-	//For receiveing data from form
-	@PostMapping("/login")
+	//For receiveing data from loginform
+	@PostMapping
     public String userInfoSubmit(@Valid User userinfo, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
             return "login";
@@ -31,18 +45,34 @@ public class UserController extends WebMvcConfigurerAdapter {
 		model.addAttribute("user", userinfo);
         return "result";
     }
-	
+	/*
+	//For connecting to facebook
+	@GetMapping
+    public String helloFacebook(Model model) {
+        if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
+            return "login";
+        }
+
+        model.addAttribute("facebookProfile", facebook.userOperations().getUserProfile());
+        PagedList<Post> feed = facebook.feedOperations().getFeed();
+        model.addAttribute("feed", feed);
+        return "result";
+    }
+	*/
 	//For displaying the form
-    @GetMapping("/login")
-    public String userInfoForm(User userinfo, Model model) {
+    @GetMapping
+    public String userInfoForm(User userinfo) {
         return "login";
     }
 	/*
 	@PostMapping("/newUser")
-	public String RequestNewUser(@Valid User userinfo, BindingResult bindingResult){
+	public String RequestNewUser(@Valid User userinfo, BindingResult bindingResult, Model model){
 		
 		
 		return "welcome";
 	}
+	
+	@GetMapping("newUser")
+	public String newUserForm(User userinfo)
 */
 }
